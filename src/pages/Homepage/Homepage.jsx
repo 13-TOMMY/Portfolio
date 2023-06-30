bc8a8f3251ae5ffa3196d770ec41fa5cbc454983
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-06-30 14:56:03 +0200

    hompage structure

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import "./Homepage.css"
+import { ThemeContext } from '../../context/ThemeContext';
 
 function Homepage() {
+  const {darkMode, setDarkMode} = useContext(ThemeContext);
   return (
-    <div>Homepage</div>
+    <div className='Homepage-container'>
+      <div className="hp-top">
+      <div className={darkMode ? "hp-top-left-dark hp-top-left" : "hp-top-left-light hp-top-left"}>
+      <h1>Front-end Developer</h1>
+      <p>Hello, i am Tomasz Lis, a enthusiastic Front-end Developer based in Krakow, Poland. I love coding and i believe in the power of continuous learning, expanding my knowledge and skills every day.</p>
+      </div>
+      <div className={darkMode ? "hp-top-right-dark hp-top-right" : "hp-top-right-light hp-top-right"}>
+      <img src="src/assets/photos/homepage-photo.png" alt="Photo of me" className='hp-top-right-img'/>
+      </div>
+      </div>
+      <div className={darkMode ? "hp-bottom-dark hp-bottom" : "hp-bottom-light hp-bottom"}>
+      <div className="hp-bottom-techStack">
+        <div className='ts-name'>
+      <h3>Tech Stack</h3>
+      </div>
+      <div className='tech-stack'>
+      <img src="src/assets/Tech-Stack/javascript.svg" alt="JavaScript icon" />
+      <img src="src/assets/Tech-Stack/react-dark.svg" alt="React icon" />
+      <img src="src/assets/Tech-Stack/html.svg" alt="HTML icon" />
+      <img src="src/assets/Tech-Stack/css.svg" alt="CSS icon" />
+      <img src="src/assets/Tech-Stack/git.svg" alt="Git icon" />
+      <img src="src/assets/Tech-Stack/oading-one.svg" alt="loading" />
+      <img src="src/assets/Tech-Stack/oading-one.svg" alt="loading" />
+      </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
