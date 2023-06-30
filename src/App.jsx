ba6551c9f62e8ad466dfacf770afe662366ef210
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-06-30 11:07:06 +0200

    Changes to the navbar, global theme button implamented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/Homepage/Homepage'
@@ -6,26 +6,32 @@ import About from './pages/About/About'
 import Portfolio from './pages/Portfolio/Portfolio'
 import Contact from './pages/Contact/Contact'
 import NavBar from './components/NavBar/NavBar'
+import {ThemeContext} from './context/ThemeContext';
+
 
 function App() {
 
+  const {darkMode, setDarkMode} = useContext(ThemeContext);
+
   return (
     <BrowserRouter>
-        <div className='viewable-area'>
-          <div className='left-side'>
+     
+        <div className={darkMode ? "dark-mode-viewArea  viewable-area" : "light-mode-viewArea  viewable-area"}>
+        <div className={darkMode ? "dark-mode-l left-side" : "light-mode-l left-side"}>
           <NavBar />
           </div>
           <div className='right-side'>
-          <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/portfolio' element={<Portfolio />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/portfolio' element={<Portfolio />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
           </div>
-          </div>
+        </div>
+      
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
