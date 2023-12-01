b06e9a380c132cf5e5512ee7477324a5ee620a8c
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-12-01 17:20:36 +0100

    added a lottie animation to the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,27 @@
 import ContactForm from "../components/ContactForm";
 import SocialMediaConnections from "../components/SocialMediaConnections";
+import Lottie from "lottie-react";
+import postBox from "../Assets/lottie/message/post-box.json";
+import { useRef } from "react";
+
 
 function Contact() {
+  const lottieRef = useRef(null);
+
   return (
     <div className="contact-page">
       <ContactForm />
-      <SocialMediaConnections/>
+      <Lottie
+            animationData={postBox}
+            loop={true}
+            lottieRef={lottieRef}
+            onComplete={() => {
+              // lottieRef.current.goToAndPlay(100, false);
+              lottieRef.current.setSpeed(0.2);
+            }}
+        className="post-box-lottie"
+          />
+      <SocialMediaConnections />
     </div>
   );
 }
